2f3f80325d71aa1d9765a9f98b13bbcda6b80ad2
lduboisd <louise.dubois-de-prisque@inria.fr>
2023-06-23 16:14:04 +0200

    chapter local storage

--- pieces.js
+++ pieces.js
@@ -1,11 +1,27 @@
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
-const pieces = await reponse.json();
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficheAvis } from "./avis.js";
+
+//Récupération des pièces eventuellement stockées dans le localStorage
+let pieces = window.localStorage.getItem('pieces');
+if (pieces === null){
+   // Récupération des pièces depuis l'API
+   const reponse = await fetch('http://localhost:8081/pieces/');
+   pieces = await reponse.json();
+   // Transformation des pièces en JSON
+   const valeurPieces = JSON.stringify(pieces);
+   // Stockage des informations dans le localStorage
+   window.localStorage.setItem("pieces", valeurPieces);
+}else{
+   pieces = JSON.parse(pieces);
+}
+
+ajoutListenerEnvoyerAvis();
 
 function genererPieces (pieces) {
     for (let i = 0; i < pieces.length; i++) {
         const article = pieces[i];
         const imageElement = document.createElement("img");
+        const pieceElement = document.createElement("article");
+
         imageElement.src = article.image;
         const nomElement = document.createElement("h2");
         nomElement.innerText = article.nom;
@@ -18,16 +34,39 @@ function genererPieces (pieces) {
         const disponibiliteElement = document.createElement("p");
         disponibiliteElement.innerText = `${article.disponibilite === true ? "En stock" : "Rupture de stock"}`;
         const sectionFiches = document.querySelector(".fiches");
-        sectionFiches.appendChild(imageElement);
-        sectionFiches.appendChild(nomElement);
-        sectionFiches.appendChild(prixElement);
-        sectionFiches.appendChild(categorieElement);
-        sectionFiches.appendChild(descriptionElement);
-        sectionFiches.appendChild(disponibiliteElement);
+
+        // ajout du bouton pour les avis
+        const avisBouton = document.createElement("button");
+
+        // add an id to the button to get the correct number in the json database
+        avisBouton.dataset.id = article.id;
+        avisBouton.textContent = "Afficher les avis";
+
+        sectionFiches.appendChild(pieceElement);
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(disponibiliteElement);
+        pieceElement.appendChild(avisBouton);
     }
+            
+    ajoutListenersAvis();
 }
 
 genererPieces (pieces);
+
+for(let i = 0; i < pieces.length; i++){
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+
+    if(avis !== null){
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficheAvis(pieceElement, avis);
+    }
+}
 
 const boutonTrier = document.querySelector(".btn-trier");
 boutonTrier.addEventListener("click", () => {
@@ -120,3 +159,9 @@ inputRange.addEventListener("change", (event) => {
     document.querySelector(".fiches").innerHTML = "";
     genererPieces(piecesFiltrees);
   });
+
+  // Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", function () {
+  window.localStorage.removeItem("pieces");
+});
